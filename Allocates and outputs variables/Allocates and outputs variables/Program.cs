83377f66755c5f6cb23fcbfd26fd4fa949f961d1
최건 <highsky2224@gmail.com>
2021-12-23 18:12:15 +0900

    두 번째 커밋
    int형 변수 a에 20을 할당해서 출력.

---
+++ Allocates and outputs variables/Allocates and outputs variables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Allocates_and_outputs_variables_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            Console.WriteLine(a);
+        }
+    }
+}
