5edd11234317448dcd6ed32c43b86ad52aabbfb8
최건 <highsky2224@gmail.com>
2021-12-23 18:20:19 +0900

    연습장.
    int형 변수 a에 20을 할당하고 출력.

---
+++ Outputs a variable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Outputs_a_variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            Console.WriteLine(a);
+        }
+    }
+}
