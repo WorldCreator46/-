7982cb0ab7b291ec599cf3bdeab89e07f0a0ed27
최건 <highsky2224@gmail.com>
2021-12-23 19:35:38 +0900

    연습장.
    상수 선언 방식.

---
+++ Constant/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int a = 50;
+
+            // a = 60; 오류 발생
+
+            Console.WriteLine(a);
+        }
+    }
+}
