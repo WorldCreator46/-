0ca72256368e9b8ddbf426fed963d7bfe9987329
최건 <highsky2224@gmail.com>
2021-12-28 16:57:26 +0900

    연습장.
    배열 및 주로 사용하는 속성과 메소드 연습.

---
+++ Arrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array
+            {
+                int[] reading = new int[6] { 4, 9, 1, 0, 21, 12 };
+                Console.WriteLine("Array");
+                for (int i = 0; i < reading.Length; i++)
+                    Console.WriteLine("reading[" + i + "]={0}", reading[i]);
+            }
+            {
+                int[] reading = new int[] { 4, 9, 1, 0, 21, 12 };
+                Console.WriteLine("\n속성: Rank={0}, Length={1}", reading.Rank, reading.Length);
+                Console.Write("Clear 메소드: ");
+                Array.Clear(reading, 0, 5);
+                Array.ForEach<int>(reading, new Action<int>(ShowValue));
+                Console.WriteLine();
+                Console.Write("Resize 메소드: ");
+                Array.Resize(ref reading, reading.Length + 3);
+                Array.ForEach<int>(reading, new Action<int>(ShowValue));
+                Console.WriteLine();
+                Console.WriteLine("indexOf 메소드: {0}", Array.IndexOf(reading, 12));
+
+                static void ShowValue(int value)
+                {
+                    Console.Write("{0} ", value);
+                }
+            }
+            // 2차원 Array
+            {
+                Console.WriteLine("\n2차원 Array");
+                int[,] reading = new int[3, 6] { { 4, 9, 1, 0, 21, 12 }, { 1, 4, 6, 0, 9, 5 }, { 4, 4, 2, 4, 0, 0 } };
+                for (int i = 0; i < reading.GetLength(0); i++)
+                    for (int j = 0; j < reading.GetLength(1); j++)
+                        Console.WriteLine("reading[" + i + ", " + j + "] = {0}", reading[i, j]);
+            }
+        }
+    }
+}
