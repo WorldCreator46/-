3f69e2d818ae79e41f812bd15b2285fd4c9d7629
최건 <highsky2224@gmail.com>
2022-01-01 14:12:36 +0900

    연습장.
    null 조건 연산자와, 문자열 보간 연습.

---
+++ Null-conditional operators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Null_conditional_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Truncate("ABCDEFGHI", 25));
+            // ?.  | 멤버에 접근하기 전 체크       
+            // ?[] | 인덱스 작업을 수행하기 전 체크 
+            // 왼쪽 피연산자의 값을 검사 후 null이 아닌 경우에만 연산자 오른쪽의 표현식 실행.
+            // null이라면 null을 반환
+        }
+        public static string Truncate(string value, int length)
+        {
+            /*
+                string result = value;
+                if (value != null)
+                {
+                    result = value.Substring(0, Math.Min(value.Length, length));
+                }
+                return result;
+            */
+            return value?.Substring(0, Math.Min(value.Length, length));
+        }
+    }
+}
---
+++ String interpolation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace String_interpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 파이썬의 f스트링과 거의 같음
+            string name = "이름";
+            var date = DateTime.Now;
+            Console.WriteLine("안녕, {0}! 오늘은 {1}, {2:HH:mm}이야.", name, date.DayOfWeek, date);
+            Console.WriteLine($"안녕, {name}! 오늘은 {date.DayOfWeek}, {date:HH:mm}이야.");
+            // 둘다 같은 결과를 내지만 아랫쪽이 더 직관적이다.
+            const int F = 20;
+            Console.WriteLine($"{Math.PI, F} 원주율의 기본형식");
+            Console.WriteLine($"{Math.PI, F:F3} 원주율을 소수점 3자리까지");
+            bool  b = true;
+            Console.WriteLine($"{(b ? "참" : "거짓")} 조건부 삼항 연산자(?:)도 소괄호로 묶어서 사용 가능!");
+            Console.WriteLine($"{{}} 중괄호는 이렇게 두번 겹쳐서");
+        }
+    }
+}
