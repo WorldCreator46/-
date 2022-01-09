13647c274f1fda3e40692005c2ac92fe0396e0c6
최건 <highsky2224@gmail.com>
2022-01-09 23:49:32 +0900

    연습.
    JSON 연습.

---
+++ GameDataSave/Save/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.IO;
+using Newtonsoft.Json;
+namespace Save
+{
+    class Program
+    {
+        public static string[] PropertyName = { "Cash", "Stock", "Furniture", "Jewelry", "NobleMetals", "RealEstate", "Car", "Bitcoin", "Accessories", "Etc" };
+        Dictionary<string, BigInteger> Property = new Dictionary<string, BigInteger>();
+        static void Main(string[] args)
+        {
+            using (StreamReader r = new StreamReader("TestJson.json"))
+            {
+                string json = r.ReadToEnd();
+            }
+        }
+        void Do()
+        {
+            foreach (string Name in PropertyName)
+            {
+                Property[Name] = BigInteger.Zero;
+            }
+        }
+    }
+}
