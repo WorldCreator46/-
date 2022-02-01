25d0adb48d65ae4772cc14c89d7e486313717559
최건 <highsky2224@gmail.com>
2022-02-01 17:44:59 +0900

    연습
    랜덤 테스트

---
+++ Test1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(new Random().Next(100, 1000));
+        }
+    }
+}
