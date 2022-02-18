c643519fe6d02abc442e7050640b668804c3e0f7
최건 <highsky2224@gmail.com>
2022-02-19 00:57:36 +0900

    테스트

---
+++ Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = 1;
+            WriteLine(t);
+        }
+    }
+}
