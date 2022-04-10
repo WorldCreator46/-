98652d821f3a2193a16971e02ba863f8ddb89b89
최건 <highsky2224@gmail.com>
2022-04-10 14:33:06 +0900

    연습
    .

---
+++ Python/Programers/완주하지 못한 선수.py
@@ -0,0 +1,8 @@
+import collections
+def solution(p, c):
+    for t in (collections.Counter(p) - collections.Counter(c)).keys():
+        return t
+
+print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
+print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
+print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
