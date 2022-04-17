172fd720c4769e93594d043c3291961bd13991f1
WorldCreator46 <96541154+WorldCreator46@users.noreply.github.com>
2022-04-17 22:04:58 +0900

    연습
    머리가 아프다

---
+++ 스킬체크 1-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+public class Solution
+{
+    static public void Main()
+    {
+        Console.WriteLine(string.Join(", ", solution(new string[] { "sun", "bed", "car" }, 1)));
+        Console.WriteLine(string.Join(", ", solution(new string[] { "abce", "abcd", "cdx" }, 2)));
+    }
+    static public string[] solution(string[] strings, int n)
+    {
+        return strings.OrderBy(word => word).ToArray().OrderBy(word => word[n]).ToArray();
+    }
+}
